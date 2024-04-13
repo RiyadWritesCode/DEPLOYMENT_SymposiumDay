0cd44b24fafbc878df985e8198d632299e0723fd
RiyadWritesCode <42694849+RiyadWritesCode@users.noreply.github.com>
2024-04-13 14:53:44 +0400

    Added gitignore and removed colorful logs.

--- server.js
+++ server.js
@@ -29,20 +29,7 @@ const app = express();
 app.use(express.json({ limit: "200kb" }));
 
 // Morgan dev preset
-const morganMiddleware = morgan(function (tokens, req, res) {
-  return [
-    chalk.hex("#ff4757").bold("🍄  Morgan --> "),
-    chalk.hex("#34ace0").bold(tokens.method(req, res)),
-    chalk.hex("#ffb142").bold(tokens.status(req, res)),
-    chalk.hex("#ff5252").bold(tokens.url(req, res)),
-    chalk.hex("#2ed573").bold(tokens["response-time"](req, res) + " ms"),
-    chalk.hex("#f78fb3").bold("@ " + tokens.date(req, res)),
-    chalk.yellow(tokens["remote-addr"](req, res)),
-    chalk.hex("#fffa65").bold("from " + tokens.referrer(req, res)),
-    chalk.hex("#1e90ff")(tokens["user-agent"](req, res)),
-    "\n",
-  ].join(" ");
-});
+app.use(morgan("dev"));
 
 // Logging to disk
 const logDirectory = path.join(__dirname, "log");
@@ -54,8 +41,6 @@ const accessLogStream = rfs.createStream("access.log", {
   path: logDirectory,
 });
 
-// Write to render logs
-app.use(morganMiddleware);
 // Morgan setup to write to file/render disk
 app.use(morgan("combined", { stream: accessLogStream }));
 
