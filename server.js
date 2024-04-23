2b48b3a3b13a491c98c00fb1a7d0192f078f6775
RiyadWritesCode <42694849+RiyadWritesCode@users.noreply.github.com>
2024-04-23 09:26:35 +0400

    Created an extensiveLogStream

--- server.js
+++ server.js
@@ -15,19 +15,53 @@ if (!fs.existsSync(logDirectory)) {
   fs.mkdirSync(logDirectory, { recursive: true });
 }
 
+const extensiveLogDirectory = "/var/extensiveLog";
+// Ensure the directory exists, create it if it doesn't
+if (!fs.existsSync(extensiveLogDirectory)) {
+  fs.mkdirSync(extensiveLogDirectory, { recursive: true });
+}
+
 // Set up rotating logs
 const accessLogStream = rfs.createStream("access.log", {
   interval: "1d", // rotate daily
   path: logDirectory,
 });
 
+// Set up rotating logs
+const accessExtensiveLogStream = rfs.createStream("access.log", {
+  interval: "1d", // rotate daily
+  path: extensiveLogDirectory,
+});
+
 // Custom Morgan token to log user email
 morgan.token("user-email", function (req) {
   return req.user ? req.user.email : "guest";
 });
+
+morgan.token("user", function (req) {
+  return req.user ? JSON.stringify(req.user) : "no req.user";
+});
+
+morgan.token("req-body", function (req) {
+  return req.body ? JSON.stringify(req.body) : "no req.body";
+});
+
+morgan.token("req-headers-authorization", function (req) {
+  return req.headers ? JSON.stringify(req.headers.authorization) : "no req.headers.authorization";
+});
+
+// Custom Morgan token to log user email
+morgan.token("req-params", function (req) {
+  return req.params ? JSON.stringify(req.params) : "no req.params";
+});
+
 const logFormat =
-  '[:date[clf]] - :remote-addr - :user-email - ":method :url HTTP/:http-version" - :status - :response-time ms - :res[content-length] - ":referrer" - ":user-agent"';
+  '[:date[web]] - :remote-addr - :user-email - ":method :url HTTP/:http-version" - :status - :response-time ms - :res[content-length] - ":referrer" - ":user-agent"';
 app.use(morgan(logFormat, { stream: accessLogStream }));
+
+const extensiveLogFormat =
+  '[:date[web]] - :remote-addr - ":method :url" - :status - :response-time ms - :res[content-length] - :user - :req-headers-authorization - :req-params - :req-body';
+app.use(morgan(extensiveLogFormat, { stream: accessExtensiveLogStream }));
 
 // Dev logs
 app.use(morgan("dev"));
